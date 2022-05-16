53a1544b1be68219ffdcd3d99a0b56939b7c5f65
Ismael.C <ismaelcadelcid@gmail.com>
2022-05-16 17:55:35 +0200

    Initial commit

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest
+{
+    @Test
+    void getName()
+    {
+        Person p = new Person("James");
+        assertEquals("James", p.getName());
+        assertEquals("John", p.getName());
+    }
+}
